38f80d951803b36ff72f9c6475746a114f76f116
AndersonRag <anderson-we@hotmail.com>
2023-03-22 20:47:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetologin/FormLogin.java
@@ -0,0 +1,49 @@
+package com.example.projetologin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+    private Button bt_entrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormLogin.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormLogin.this,TelaPrincipal.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        bt_entrar = findViewById(R.id.bt_entrar);
+    }
+}
