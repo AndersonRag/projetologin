acd3cf539e2ee4e23f8877b0015bc47f5a7ea487
AndersonRag <anderson-we@hotmail.com>
2023-03-25 10:03:51 -0300

    atualização do dia 23/03/23

--- app/src/main/java/com/example/projetologin/FormCadastro.java
+++ app/src/main/java/com/example/projetologin/FormCadastro.java
@@ -1,15 +1,37 @@
 package com.example.projetologin;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class FormCadastro extends AppCompatActivity {
+public class FormCadastro extends AppCompatActivity implements View.OnClickListener{
+
+    androidx.appcompat.widget.AppCompatButton bt_cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        bt_cadastrar = (androidx.appcompat.widget.AppCompatButton) findViewById(R.id.bt_cadastar);
+        bt_cadastrar.setOnClickListener(this);
+
         getSupportActionBar().hide();
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        String msg = "Cadastrado Realizado!";
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+        dialogo.setMessage(msg);
+        dialogo.setTitle("Cadastro");
+        dialogo.setNeutralButton("Ok",null);
+        dialogo.show();
+
     }
 }
